b80781825d1e4551f4edbc59970b39598c81c842
Adil Farhan <dnkrealestate2022@gmail.com>
2026-03-04 12:46:49 +0400

    update meta pixels

--- app/layout.js
+++ app/layout.js
@@ -83,6 +83,37 @@ export default async function RootLayout({ children }) {
          {/* ✅ Google Tag Manager */}
       <GoogleTagManager gtmId="GTM-WN4SJSW3" />
 
+      {/* ✅ Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '702378121580941'); https://connect.facebook.net/en_US/fbevents.js');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        {/* ✅ Meta Pixel NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=702378121580941&ev=PageView&noscript=1`}
+            alt="meta-pixel"
+          />
+        </noscript>
+
         {/* ✅ JSON-LD (SEO safe) */}
         <Script
           id="jsonld-org"
